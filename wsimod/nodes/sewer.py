2c0599faa36ebbaf681f9c69a07091c66ddd2347
Dobson <bdobson@ic.ac.uk>
2022-03-16 16:57:48 +0000

    Update sewer.py
    Think this is a mistake in the foul sewer... whoops

--- wsimod/nodes/sewer.py
+++ wsimod/nodes/sewer.py
@@ -125,7 +125,7 @@ class EnfieldFoulSewer(Sewer):
             exchange_v = min((1 - self.storm_exchange) * self.sewer_tank.capacity, 
                              self.sewer_tank.active_storage['volume'])
             exchange = self.v_change_vqip(self.sewer_tank.active_storage, exchange_v)
-            remaining = self.push_distributed(exchange)
+            remaining = self.push_distributed(exchange, of_type = ['Sewer'])
             sent_to_exchange = self.v_change_vqip(self.sewer_tank.active_storage, exchange_v - remaining['volume'])
             sent_to_exchange_ = self.sewer_tank.pull_storage(sent_to_exchange)
             
