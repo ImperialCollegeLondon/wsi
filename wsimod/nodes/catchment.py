3528b52878c5b2fffafffffb9bd8554a0a974e81
barneydobson <barnaby.dobson1@gmail.com>
2022-09-13 15:05:16 +0100

    Document waste and catchment

--- wsimod/nodes/catchment.py
+++ wsimod/nodes/catchment.py
@@ -15,6 +15,16 @@ class Catchment(Node):
                         name,
                         data_input_dict = {},
                         ):
+        """Node that reads input data to create VQIPs that are pushed downstream and 
+        tracks abstractions made from the node, adjusting pushes accordingly.
+
+        Args:
+            name (str): Node name
+            data_input_dict (dict, optional): Dictionary of data inputs relevant for 
+                the node. Keys are tuples where first value is the name of the 
+                variable to read from the dict and the second value is the time. 
+                Defaults to {}.
+        """
         
         #Update args
         super().__init__(name)
@@ -30,6 +40,11 @@ class Catchment(Node):
         self.mass_balance_in.append(lambda : self.get_flow())
         
     def get_flow(self):
+        """Read volume data (assumed in m3/s), convert to m3/dt, read pollutant data
+
+        Returns:
+            vqip (dict): Return read data as a VQIP
+        """
         #TODO (if used) - note that if flow is < float accuracy then it won't 
         #get pushed, and the pollutants will 'disappear', causing a mass balance error
         vqip = {'volume' : self.data_input_dict[('flow',
@@ -41,22 +56,42 @@ class Catchment(Node):
         return vqip
     
     def route(self):
-        #Route excess flow onwards
+        """Send any water that has not already been abstracted downstream
+        """
+        #Get amount of water
         avail = self.pull_avail()
+        #Route excess flow onwards
         reply = self.push_distributed(avail)
         if reply['volume'] > constants.FLOAT_ACCURACY:
             print('Catchment unable to route')
     
     def pull_avail(self):
+        """Water available for abstraction (Read data and subtract pre-existing 
+        abstractions)
+
+        Returns:
+            avail (dict): A VQIP of water available for abstraction
+        """
+
         #Get available vqip
         avail = self.get_flow()
         
+        #Remove abstractions already made
         for name, arc in self.out_arcs.items():
             avail = self.v_change_vqip(avail, avail['volume'] - arc.vqip_in['volume'])
         
         return avail
     
     def pull_check_abstraction(self, vqip = None):
+        """Check wrapper for pull_avail that updates response if VQIP is given
+
+        Args:
+            vqip (dict, optional): A VQIP that is compared with pull_avail and the 
+                minimum is returned. Only the 'volume' key is used. Defaults to None.
+
+        Returns:
+            avail (dict): A VQIP of water available for abstraction
+        """
         #Respond to abstraction check request
         avail = self.pull_avail()
         
@@ -67,6 +102,14 @@ class Catchment(Node):
         return avail
     
     def pull_set_abstraction(self, vqip):
+        """Request set wrapper for pull_avail where VQIP is specified
+
+        Args:
+            vqip (dict): A VQIP of water to pull. Only the 'volume' key is used.
+
+        Returns:
+            avail (dict): A VQIP of water abstracted
+        """
         #Respond to abstraction set request
         avail = self.pull_avail()
         avail = self.v_change_vqip(avail, 
--- wsimod/nodes/waste.py
+++ wsimod/nodes/waste.py
@@ -9,6 +9,11 @@ from wsimod.core import constants
 #TODO call this outlet not waste
 class Waste(Node):
     def __init__(self, name):
+        """Outlet node that can receive any amount of water by pushes
+
+        Args:
+            name (str): Node name
+        """
         #Update args
         super().__init__(name)
         
@@ -22,11 +27,26 @@ class Waste(Node):
         self.mass_balance_out.append(self.total_in)
         
     def push_set_accept(self, vqip):
-        #Returns all request accepted
+        """Push set function that accepts all water
+
+        Args:
+            vqip (dict): A VQIP that has been pushed (ignored)
+
+        Returns:
+            (dict): An empty VQIP, indicating all water was received
+        """
+
         return self.empty_vqip()
     
     def push_check_accept(self, vqip = None):
-        #Returns unbounded available push capacity
+        """Push check function that accepts all water
+
+        Args:
+            vqip (dict, optional): A VQIP that has been pushed (ignored)
+
+        Returns:
+            (dict): VQIP or an unbounded capacity, indicating all water can be received
+        """
         if not vqip:
             vqip = self.empty_vqip()
             vqip['volume'] = constants.UNBOUNDED_CAPACITY
