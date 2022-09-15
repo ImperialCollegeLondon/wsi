0057159d73222acd31bfa373970267876dffa9c1
barneydobson <barnaby.dobson1@gmail.com>
2022-09-15 17:07:50 +0100

    Update demand.py
    documentation

--- wsimod/nodes/demand.py
+++ wsimod/nodes/demand.py
@@ -21,7 +21,31 @@ class Demand(Node):
                         constant_temp = 30,
                         constant_weighting = 0.2,
                         ):
-        #Default parameters
+        """Node that generates and moves water associated with population. 
+
+        Args:
+            name (str): node name
+            population (float, optional): population of node. Defaults to 1.
+            pollutant_load (dict, optional): Mass per person per timestep of 
+                different pollutants generated. Defaults to {}.
+            per_capita (float, optional): Volume per person per timestep of water 
+                used. Defaults to 0.12.
+            gardening_efficiency (float, optional): Value between 0 and 1 that 
+                translates irrigation demand from GardenSurface into water requested 
+                from the distribution network. Should account for percent of garden 
+                that is irrigated and the efficacy of people in meeting their garden 
+                water demand. Defaults to 0.6*0.7.
+            data_input_dict (dict, optional):  Dictionary of data inputs relevant for 
+                the node (temperature). Keys are tuples where first value is the name of the variable to read from the dict and the second value is the 
+                time. Defaults to {}
+            constant_temp (float, optional): A constant temperature associated with 
+                generated water. Defaults to 30
+            constant_weighting (float, optional): Proportion of temperature that is 
+                made up from by cconstant_temp. Defaults to 0.2.
+        """
+        #TODO should temperature be defined in pollutant dict?
+        #TODO a lot of this should probably be moved to ResidentialDemand
+        #Assign parameters
         self.gardening_efficiency = gardening_efficiency
         self.population = population
         self.per_capita = per_capita
@@ -45,12 +69,18 @@ class Demand(Node):
         #Mass balance
         # Because we assume demand is always satisfied
         # received water 'disappears' for mass balance
-        # and consumed water 'appears'
+        # and consumed water 'appears' (this makes)
+        # introduction of pollutants easy
         self.mass_balance_in.append(lambda : self.total_demand)
         self.mass_balance_out.append(lambda : self.total_backup)
         self.mass_balance_out.append(lambda : self.total_received) 
         
     def create_demand(self):
+        """Function to call get_demand, which should return a dict with keys that 
+        match the keys in directions. A dict that determines how to push_distributed 
+        the generated wastewater/garden irrigation. Water is drawn from attached 
+        nodes.
+        """
         demand = self.get_demand()
         total_requested = 0
         for dem in demand.values():
@@ -68,9 +98,6 @@ class Demand(Node):
         
         #Send water where it needs to go
         for key, item in demand.items():
-            #Ensure water has the concentration that was received
-            # item = self.v_change_vqip(self.total_received, item['volume'])
-            # demand[key] = item #put back in dict for mass balance checking
             
             #Distribute
             remaining = self.push_distributed(item,
@@ -88,22 +115,45 @@ class Demand(Node):
                                               dem)
                 
     def get_constant_demand(self):
+        """Holder function to enable constant demand generation
+
+        Returns:
+            (dict): A VQIP that will contain constant demand
+        """
         #TODO read/gen demand
         return self.empty_vqip()
     
     def end_timestep(self):
+        """Reset state variable trackers
+        """
         self.total_demand = self.empty_vqip()
         self.total_backup = self.empty_vqip()
         self.total_received = self.empty_vqip()
         
-class NonResidentialDemand(Demand): 
+class NonResidentialDemand(Demand):
+    """Holder class to enable non-residential demand generation
+    """
         
     def get_demand(self):
-        return self.get_constant_demand()             
+        """Holder function to call get_constant_demand
+
+        Returns:
+            (dict): A dict of VQIPs, where the keys match with directions 
+                in Demand/create_demand
+        """
+        return {'house' : self.get_constant_demand()}
 
 class ResidentialDemand(Demand):
+    """Subclass of demand with functions to handle internal and external water use
+    """
     
     def get_demand(self):
+        """Overwrite get_demand and replace with custom functions
+
+        Returns:
+            (dict): A dict of VQIPs, where the keys match with directions 
+                in Demand/create_demand
+        """
         water_output = {}        
                 
         water_output['garden'] = self.get_garden_demand()        
@@ -113,49 +163,75 @@ class ResidentialDemand(Demand):
 
     
     def get_garden_demand(self):
+        """Calculate garden water demand in the current timestep by get_connected
+        to all attached land nodes. This check should return garden water demand.
+        Applies irrigation coefficient. Can function when a single population node is 
+        connected to multiple land nodes, however, the capacity and preferences of 
+        arcs should be updated to reflect what is possible based on area.
+
+        Returns:
+            vqip (dict): A VQIP of garden water use (including pollutants) to be 
+                pushed to land
         """
-        Calculate garden water demand in the current timestep by get_connected
-        to all attached land nodes. The preference along the arc between a 
-        demand and land node should be the percentage of garden area in a 
-        given land node that consists of gardens in 'self' demand node.
-        
-        Returns
-        -------
-        vqip : Blended vqip of garden water use (including pollutants) to be 
-            pushed to land
-            
-        Example
-        -------
-        garden_water_use = self.get_garden_demand()
-        """
+        #Get garden water demand
         excess = self.get_connected(direction = 'push',
                                     of_type = 'Land', 
                                     tag = ('Demand', 
                                            'Garden')
                                     )['avail']
         
+        #Apply garden_efficiency
         excess = self.excess_to_garden_demand(excess)
+
+        #Apply any pollutants
         vqip = self.apply_gardening_pollutants(excess)
         return vqip
     
     def apply_gardening_pollutants(self, excess):
-        #TODO do
+        """Holder function to apply pollutants (i.e., presumably fertiliser) to the garden.
+
+        Args:
+            excess (float): A volume of water applied to a garden
+
+        Returns:
+            (dict): A VQIP of water that includes pollutants to be sent to land
+        """
+        #TODO Fertilisers are currently applied in the land node... which is preferable?
         vqip = self.empty_vqip()
         vqip['volume'] = excess
         return vqip
         
         
     def excess_to_garden_demand(self, excess):
+        """Apply garden_efficiency
+
+        Args:
+            excess (float): Volume of water required to satisfy garden irrigation
+
+        Returns:
+            (float): Amount of water actually applied to garden
+        """
         #TODO Anything more than this needed?
-        # (yes - population presence if included!)
+        # (yes - population presence if eventually included!)
         
         return excess * self.gardening_efficiency
     
     def get_house_demand(self):
+        """Per capita calculations for household wastewater generation. Applies 
+        weighted temperature calculation
+
+        Returns:
+            (dict): A VQIP containg foul water
+        """
+        #TODO water that is consumed but not sent onwards as foul
+        #Total water required
         consumption = self.population * self.per_capita
+        #Apply pollutants
         foul = self.copy_vqip(self.pollutant_load)
+        #Scale to population
         for pol in constants.ADDITIVE_POLLUTANTS:
             foul[pol] *= self.population
+        #Update volume and temperature (which is weighted based on air temperature and constant_temp)
         foul['volume'] = consumption
         foul['temperature'] = self.get_data_input('temperature') * (1 - self.constant_weighting) + self.constant_temp * self.constant_weighting
         return foul
