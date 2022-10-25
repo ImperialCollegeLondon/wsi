563f40ec026f2ed077f9702cf792b2fa2efc6a2d
barneydobson <barnaby.dobson1@gmail.com>
2022-10-25 11:16:50 +0100

    Update test_arcs.py
    finish arc unit tests

--- tests/test_arcs.py
+++ tests/test_arcs.py
@@ -14,7 +14,7 @@ from wsimod.core import constants
 from wsimod.nodes.nodes import Node, Tank, ResidenceTank, DecayTank, QueueTank, DecayQueueTank
 from wsimod.nodes.waste import Waste
 from wsimod.nodes.storage import Storage
-from wsimod.arcs.arcs import Arc, QueueArc, AltQueueArc, DecayArc
+from wsimod.arcs.arcs import Arc, QueueArc, AltQueueArc, DecayArc, DecayArcAlt
 constants.set_simple_pollutants()
 
     
@@ -121,9 +121,51 @@ class MyTestClass(TestCase):
         
         reply = arc1.send_pull_request({'volume' : 10})
         self.assertDictAlmostEqual(d2, reply, 16)
+    
+    def test_enter_arcq(self):
+        node1 = Node(name = '1')
+        node2 = Waste(name = '2')
         
+        arc1 = QueueArc(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        )
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
         
-    def test_queue_arc_pull(self):
+        request = arc1.enter_arc({'vqip' : d1, 'time' : 1}, 'pull', 'hi')
+        self.assertEqual(5, request['average_flow'])
+        self.assertEqual(5, arc1.flow_in)
+        self.assertEqual('pull', request['direction'])
+        self.assertEqual('hi', request['tag'])
+        self.assertDictAlmostEqual(d1,request['vqip'])
+        self.assertDictAlmostEqual(d1,arc1.vqip_in)
+    
+    def test_enter_q_arcq(self):
+        node1 = Node(name = '1')
+        node2 = Waste(name = '2')
+        
+        arc1 = QueueArc(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        )
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
+        req = {'vqip' : d1, 'time' : 1}
+        arc1.enter_queue(req, direction = 'pull', tag='hi')
+
+        self.assertEqual(5, arc1.queue[0]['average_flow'])
+        self.assertEqual(5, arc1.flow_in)
+        self.assertEqual('pull', arc1.queue[0]['direction'])
+        self.assertEqual('hi', arc1.queue[0]['tag'])
+        self.assertDictAlmostEqual(d1,arc1.queue[0]['vqip'])
+        self.assertDictAlmostEqual(d1,arc1.vqip_in)
+
+    def test_queue_arc_push(self):
         node1 = Node(name = '1')
         node2 = Waste(name = '2')
         
@@ -139,7 +181,161 @@ class MyTestClass(TestCase):
                 'temperature' : 15}
         
         arc1.send_push_request(d1)
-        pass
+        
+        self.assertDictAlmostEqual(d1,arc1.vqip_in)
+        self.assertDictAlmostEqual(d1,arc1.queue[0]['vqip'])
+        
+        arc1.end_timestep()
+        
+        d2 = {'volume' : 0,
+              'phosphate' : 0,
+              'temperature' : 0}
+        self.assertDictAlmostEqual(d2,arc1.vqip_in)
+        self.assertDictAlmostEqual(d1,arc1.queue[0]['vqip'])
+        
+        _ = arc1.update_queue(direction = 'push')
+        self.assertDictAlmostEqual(d1,arc1.vqip_out)
+    
+    def test_queue_arc_pull(self):
+        node1 = Node(name = '1')
+        node2 = Storage(name = '2', capacity =10,initial_storage = 5)
+        
+        arc1 = QueueArc(in_port = node2, 
+                        out_port = node1, 
+                        name = 'arc1',
+                        capacity = 10,
+                        number_of_timesteps = 1
+                        )
+        
+        d1 = {'volume' : 2.5}
+        d2 = {'volume' : 0,
+              'phosphate' : 0,
+              'temperature' : 0}
+        
+        reply = arc1.send_pull_request(d1)
+        
+        self.assertDictAlmostEqual(d2,reply)
+        
+        d2['volume'] = 2.5
+        
+        self.assertDictAlmostEqual(d2,arc1.queue[0]['vqip'])
+        self.assertDictAlmostEqual(d2,arc1.vqip_in)
+        
+        arc1.end_timestep()
+        
+        self.assertDictAlmostEqual(d2,arc1.queue[0]['vqip'])
+        
+        _ = arc1.update_queue(direction = 'pull')
+        self.assertDictAlmostEqual(d2,arc1.vqip_out)
+    
+    def test_altqueue_arc_sum(self):
+        node1 = Node(name = '1')
+        node2 = Waste(name = '2')
+        
+        arc1 = AltQueueArc(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        number_of_timesteps = 1
+                        )
+        
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
+        
+        arc1.queue[1] = d1
+        arc1.queue[0] = d1
+        
+        d2 = {'volume' : 20,
+                'phosphate' : 0.0002,
+                'temperature' : 15}
+        
+        self.assertDictAlmostEqual(d2, arc1.alt_queue_arc_sum())
+    
+    def test_enter_q_arcalt(self):
+        node1 = Node(name = '1')
+        node2 = Waste(name = '2')
+        
+        arc1 = AltQueueArc(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        )
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
+        req = {'vqip' : d1, 'time' : 1}
+        arc1.enter_queue(req, direction = 'push', tag='hi')    
+        
+        self.assertEqual(5, arc1.flow_in)
+        self.assertDictAlmostEqual(d1,arc1.queue[1])
+        self.assertDictAlmostEqual(d1,arc1.vqip_in) 
+        
+        arc1.end_timestep()
+        
+        self.assertEqual(0, arc1.flow_in)
+        self.assertDictAlmostEqual(d1,arc1.queue[0])
+        
+        d2 = {'volume' : 0,
+              'phosphate' : 0,
+              'temperature' : 0}
+        self.assertDictAlmostEqual(d2,arc1.vqip_in)
+        
+        arc1.update_queue()
+        self.assertDictAlmostEqual(d2,arc1.queue[0])
+        self.assertDictAlmostEqual(d1,arc1.vqip_out)
+    
+    def test_decay_arc(self):
+        node1 = Node(name = '1',
+                     data_input_dict = {('temperature', 1) : 10})
+        node2 = Waste(name = '2')
+        node1.t = 1
+        arc1 = DecayArc(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        decays = {'phosphate' : {'constant' : 0.005,
+                                                 'exponent' : 1.005}},
+                        number_of_timesteps = 1
+                        )
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
+        
+        reply = arc1.send_push_request(d1)
+        self.assertDictAlmostEqual(d1,arc1.vqip_in)
+        d1['phosphate'] = d1['phosphate'] * (1 - 0.005 * 1.005 **(10-20))
+        self.assertDictAlmostEqual(d1,arc1.queue[0]['vqip'])
+        
+        arc1.end_timestep()
+        d1['phosphate'] = d1['phosphate'] * (1 - 0.005 * 1.005 **(10-20))
+        self.assertDictAlmostEqual(d1,arc1.queue[0]['vqip'])
+        
+    def test_decay_arc_alt(self):
+        node1 = Node(name = '1',
+                     data_input_dict = {('temperature', 1) : 10})
+        node2 = Waste(name = '2')
+        node1.t = 1
+        arc1 = DecayArcAlt(in_port = node1, 
+                        out_port = node2, 
+                        name = 'arc1',
+                        capacity = 10,
+                        number_of_timesteps = 1,
+                        decays = {'phosphate' : {'constant' : 0.005,
+                                                 'exponent' : 1.005}}
+                        )
+        d1 = {'volume' : 10,
+                'phosphate' : 0.0001,
+                'temperature' : 15}
+        
+        reply = arc1.send_push_request(d1)
+        self.assertDictAlmostEqual(d1,arc1.vqip_in)
+        d1['phosphate'] = d1['phosphate'] * (1 - 0.005 * 1.005 **(10-20))
+        self.assertDictAlmostEqual(d1,arc1.queue[1])
+        
+        arc1.end_timestep()
+        d1['phosphate'] = d1['phosphate'] * (1 - 0.005 * 1.005 **(10-20))
+        self.assertDictAlmostEqual(d1,arc1.queue[0])
         
 if __name__ == "__main__":
     unittest.main()
