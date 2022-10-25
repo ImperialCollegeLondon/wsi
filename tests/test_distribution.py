e74c2280d4c9eac33b3175515bf3152378752d69
barneydobson <barnaby.dobson1@gmail.com>
2022-10-25 15:43:44 +0100

    Create test_distribution.py

---
+++ tests/test_distribution.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 10:35:51 2022
+
+@author: Barney
+"""
+
+import pytest
+# import pytest
+import unittest
+from unittest import TestCase
+
+from wsimod.core import constants
+from wsimod.nodes.waste import Waste
+from wsimod.nodes.distribution import UnlimitedDistribution
+from wsimod.nodes.nodes import Node
+from wsimod.nodes.land import Land
+from wsimod.arcs.arcs import Arc
+from pandas import to_datetime
+constants.set_simple_pollutants()
+
+    
+
+class MyTestClass(TestCase):
+    
+    def assertDictAlmostEqual(self, d1, d2, accuracy = 19):
+        for d in [d1, d2]:
+            for key, item in d.items():
+                d[key] = round(item, accuracy)
+        self.assertDictEqual(d1, d2)
+    
+    def test_unlimited(self):
+        distribution = UnlimitedDistribution(name='')
+        
+        d1 = {'volume' : 1000,
+              'phosphate' : 0,
+              'temperature' : 0}
+        reply = distribution.pull_set_unlimited(d1)
+        
+        self.assertDictAlmostEqual(d1, reply)
+        self.assertDictAlmostEqual(d1, distribution.supplied)
+        
+        reply = distribution.pull_set_unlimited(d1)
+        
+        d1['volume'] = 2000
+        self.assertDictAlmostEqual(d1, distribution.supplied)
+        
+if __name__ == "__main__":
+    unittest.main()
+    
