b9ad9059f205d74dfe2c6d9023008f0f1cf82a70
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2023-11-04 15:49:51 +0000

    More tweak of ruff config

---
+++ wsimod/nodes/__init__.py
@@ -0,0 +1,17 @@
+# ruff: noqa: F401
+from wsimod.nodes.catchment import Catchment
+from wsimod.nodes.demand import Demand, NonResidentialDemand, ResidentialDemand
+from wsimod.nodes.distribution import Distribution, UnlimitedDistribution
+from wsimod.nodes.land import Land
+from wsimod.nodes.nodes import Node
+from wsimod.nodes.sewer import EnfieldFoulSewer, Sewer
+from wsimod.nodes.storage import (
+    Groundwater,
+    QueueGroundwater,
+    Reservoir,
+    River,
+    RiverReservoir,
+    Storage,
+)
+from wsimod.nodes.waste import Waste
+from wsimod.nodes.wtw import FWTW, WTW, WWTW
