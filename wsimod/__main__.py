0af6a9fa0ed1b1f3ec7aad68008d69081a8f8713
Diego Alonso Álvarez <d.alonso-alvarez@imperial.ac.uk>
2023-11-13 17:32:58 +0000

    Add initial arguments for the CLI

--- wsimod/__main__.py
+++ wsimod/__main__.py
@@ -1,10 +1,37 @@
 """The entry point for the myproject program."""
 from argparse import ArgumentParser
+from pathlib import Path
 
 
 def create_parser() -> ArgumentParser:
     """Create the CLI argument parser."""
     parser = ArgumentParser(prog="WSIMOD")
+    parser.add_argument(
+        "settings",
+        type=Path,
+        help="Path to the WSIMOD input file, in TOML format.",
+    )
+    parser.add_argument(
+        "--inputs",
+        "-i",
+        type=Path,
+        help="Base directory for all input files. If present, overwrites value in the"
+        " settings file.",
+    )
+    parser.add_argument(
+        "--outputs",
+        "-o",
+        type=Path,
+        help="Base directory for all output files. If present, overwrites value in the"
+        " settings file.",
+    )
+    parser.add_argument(
+        "--force-output",
+        "-f",
+        type=bool,
+        help="Overwrite contents of output directory, if it exists. Default to False.",
+        default=False,
+    )
 
     return parser
 
