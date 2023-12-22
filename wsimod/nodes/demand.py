69ad4b68c9b8b9e6b8d7086d8fdf9b545d2d4087
liuly12 <leyang.liu@hotmail.com>
2023-12-22 10:23:49 +0000

    Add new node - timevaryingdemand

--- wsimod/nodes/demand.py
+++ wsimod/nodes/demand.py
@@ -280,3 +280,32 @@ class ResidentialDemand(Demand):
         foul['volume'] = consumption
         foul['temperature'] = self.get_data_input('temperature') * (1 - self.constant_weighting) + self.constant_temp * self.constant_weighting
         return foul
+
+class TimevaryingDemand(Demand):
+    def __init__(self,
+                        **kwargs):
+        """Node that generates time-varying water demand specified by data input.
+
+        Args:
+            data_input_dict (dict, optional):  This must contains 'demand' along with the original 
+            input vairables (e.g., 'temperature')
+        
+        Functions intended to call in orchestration:
+            create_demand
+        """
+        #Update args
+        
+        super().__init__(**kwargs)
+    
+    def get_demand(self):
+        """Holder function to enable constant demand generation
+
+        Returns:
+            (dict): A VQIP that will contain constant demand
+        """
+        #TODO read/gen demand
+        self.constant_demand = self.get_data_input('demand')
+        pol = self.v_change_vqip(self.empty_vqip(), self.constant_demand)
+        for key, item in self.pollutant_load.items():
+            pol[key] = item
+        return {'default' : pol}
