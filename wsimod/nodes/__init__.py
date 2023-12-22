6fcbd6e01088fce2288db0d9e8a065af96d5566b
liuly12 <leyang.liu@hotmail.com>
2023-12-22 10:23:02 +0000

    Include new classes

--- wsimod/nodes/__init__.py
+++ wsimod/nodes/__init__.py
@@ -1,8 +1,8 @@
 from wsimod.nodes.catchment import Catchment
-from wsimod.nodes.demand import Demand, ResidentialDemand, NonResidentialDemand
+from wsimod.nodes.demand import Demand, ResidentialDemand, NonResidentialDemand, TimevaryingDemand
 from wsimod.nodes.distribution import Distribution, UnlimitedDistribution
 from wsimod.nodes.land import Land
-from wsimod.nodes.storage import Storage, Reservoir, River, RiverReservoir, Groundwater, QueueGroundwater
+from wsimod.nodes.storage import Storage, Reservoir, River, RiverReservoir, Groundwater, Groundwater_h, QueueGroundwater, Lake
 from wsimod.nodes.nodes import Node
 from wsimod.nodes.sewer import Sewer, EnfieldFoulSewer
 from wsimod.nodes.waste import Waste
